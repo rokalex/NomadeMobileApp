fa4b548b740a8e1074c46180911e80ebabfb37c9
Alexandre Rocca <alexandrerocca@MacBook-Pro-de-Alexandre.local>
2017-06-17 12:21:36 +0200

    material header footer comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
+import { MaterializeModule } from 'angular2-materialize';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './module/layout/header/header.component';
+import { FooterComponent } from './module/layout/footer/footer.component';
+import { BodyComponent } from './module/layout/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
+    MaterializeModule,
     BrowserModule
   ],
   providers: [],
